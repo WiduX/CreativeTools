380e3eb16b85fdae2e32332e97bcb5362dfa36d6
WiduX <WiduX@live.com>
2013-03-15 22:34:13 -0700

    Assigned Textures for the Block Assigned Creative mode Tab for the Block

--- common/widux/creativetools/BlockCreative.java
+++ common/widux/creativetools/BlockCreative.java
@@ -1,21 +1,93 @@
 package widux.creativetools;
 
+import java.util.List;
+import java.util.Random;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BlockCreative extends BlockContainer
 {
 	
+	Icon[][] textures;
+	
 	public BlockCreative(int ID)
 	{
 		super(ID, Material.iron);
+		this.setCreativeTab(CreativeTabs.tabRedstone);
 	}
 
 	public TileEntity createNewTileEntity(World world)
 	{
 		return new TileEntityCreative();
 	}
+	
+	@SideOnly(Side.CLIENT)
+    public void func_94332_a(IconRegister iconRegister)
+    {
+        this.textures = new Icon[3][6];
+
+        for (int meta = 0; meta < 3; meta++)
+        {
+        	for(int side = 0; side < 6; side++)
+        	{
+        		this.textures[meta][side] = iconRegister.func_94245_a(ModInfo.INTERNAL_NAME + ":" + getBlockNameFromMeta(meta) + "_" + side);
+        	}
+        }
+    }
+	
+	private String getBlockNameFromMeta(int meta)
+	{
+		switch(meta)
+		{
+		case 0:
+			return "ItemBlock";
+		case 1:
+			return "LiquidBlock";
+		case 2:
+			return "EnergyBlock";
+		default:
+			return null;
+		}
+	}
+	
+    public Icon getBlockTexture(IBlockAccess iba, int x, int y, int z, int blockSide)
+    {
+    	int meta = iba.getBlockId(x, y, z);
+    	return this.textures[meta][blockSide];
+    }
+	
+    public Icon getBlockTextureFromSideAndMetadata(int side, int meta)
+    {
+    	return this.textures[meta][side];
+    }
+    
+	public int idDropped(int meta, Random random, int i)
+	{
+		return 0;
+	}
+	
+	public int quantityDropped(Random random)
+	{
+		return 0;
+	}
+	
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+	@SideOnly(Side.CLIENT)
+    public void getSubBlocks(int unknown, CreativeTabs tab, List subItems)
+    {
+    	for (int meta = 0; meta < 3; meta++)
+    	{
+    		subItems.add(new ItemStack(this, 1, meta));
+    	}
+    }
 	
 }
