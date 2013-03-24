f1891a79edfa6ab4fa1afd1fac7db1f202be03dc
Emile Bolduc <Emile Bolduc@Emile-Computer>
2013-03-23 22:19:28 -0400

    Fixed stuff.

--- common/widux/creativetools/TileEntityPowerDuplicator.java
+++ common/widux/creativetools/TileEntityPowerDuplicator.java
@@ -1,11 +1,14 @@
 package widux.creativetools;
 
+import buildcraft.api.core.SafeTimeTracker;
 import buildcraft.api.power.IPowerProvider;
+import buildcraft.api.power.IPowerReceptor;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.common.ForgeDirection;
 
-public class TileEntityPowerDuplicator extends TileEntity// implements IPowerProvider
+public class TileEntityPowerDuplicator extends TileEntity implements IPowerProvider
 {
 	
 	private PowerMode currentMode = PowerMode.NONE;
@@ -70,6 +73,85 @@ public class TileEntityPowerDuplicator extends TileEntity// implements IPowerPro
 	public boolean isUsableByPlayer(EntityPlayer player)
 	{
 		return this.worldObj.getBlockTileEntity(this.xCoord, this.yCoord, this.zCoord) != this ? false : player.getDistanceSq((double)this.xCoord + 0.5D, (double)this.yCoord + 0.5D, (double)this.zCoord + 0.5D) <= 64.0D;
+	}
+
+	
+	public int getLatency() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public int getMinEnergyReceived() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public int getMaxEnergyReceived() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public int getMaxEnergyStored() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public int getActivationEnergy() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public float getEnergyStored() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	public void configure(int latency, int minEnergyReceived,
+			int maxEnergyReceived, int minActivationEnergy, int maxStoredEnergy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void configurePowerPerdition(int powerLoss, int powerLossRegularity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public boolean update(IPowerReceptor receptor) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+	public boolean preConditions(IPowerReceptor receptor) {
+		return true;
+	}
+
+	
+	public float useEnergy(float min, float max, boolean doUse) {
+		//K.
+		return 0;
+	}
+	
+	public void receiveEnergy(float quantity, ForgeDirection from) {
+		//Doesn't do SHIT!
+	}
+	
+	public boolean isPowerSource(ForgeDirection from) {
+		return true;
+	}
+
+	public SafeTimeTracker getTimeTracker() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
